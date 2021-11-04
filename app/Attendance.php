1e3900ef18088440af50ae05dc5ee29847bbf5e9
alfimaulana55 <alfimaulana55@gmail.com>
2021-11-04 10:35:07 +0700

    Membuat Halaman Index dan Show Attendance

--- app/Attendance.php
+++ app/Attendance.php
@@ -12,4 +12,8 @@ class Attendance extends Model
     {
         return $this->hasMany(AttendanceDetail::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
